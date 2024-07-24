0c4f6812ae5da6ff06475d64db885a58c8adaf8e
Athira <athirakrishnanr94@gmail.com>
2024-07-24 18:50:38 +0530

    first commit

---
+++ basic/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'basic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='athira',
+    maintainer_email='athira@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = basic.pub:main',
+            'listener = basic.sub:main'
+
+        ],
+    },
+)
